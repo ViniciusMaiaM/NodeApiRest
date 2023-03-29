1e0a3678fb8d63ad168c1ed0d6c022422b715b16
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-29 11:15:19 -0300

    feat(server): Update server ts
    and add index ts for routes

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import { router } from './routes/';
 
 const server = express();
 
-server.get('/', (req, res) => res.send('Funcionou'));
+server.use(router);
 
 export {server};
