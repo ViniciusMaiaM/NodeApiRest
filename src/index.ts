ce4c7a9c7120421ecf3dbc2f6ada7d8f966771ed
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-29 12:47:59 -0300

    feat(index): receive port from env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { server } from './server/Server';
 
-server.listen(3333, () => console.log('O app está rodando!'));
+// Caso a porta localizada no env seja undefined a porta utilizada será a 3333
+server.listen(process.env.PORT || 3333, () =>{
+  console.log(`O app está rodando na porta ${process.env.PORT || 3333}`);
+});
