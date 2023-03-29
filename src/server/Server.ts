574d98824ee8313db8cb061210c21f929c4818ef
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-29 12:49:14 -0300

    feat(server): Add dotenv config

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { router } from './routes/';
+import 'dotenv/config';
 
 const server = express();
 
+server.use(express.json());
 server.use(router);
 
 export {server};
