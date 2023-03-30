7ad86986d7e4c85e7cdd2038fde3902dffba9110
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-29 21:38:53 -0300

    feat(cities): add error validation

--- src/server/controllers/Cities/createCityController.ts
+++ src/server/controllers/Cities/createCityController.ts
@@ -1,14 +1,37 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+// Yup é um package para schema e validação em tempo de execução
+import * as yup from 'yup';
 
 interface ICity{
   name: string;
+  state: string;
 }
 
+const bodyValidation: yup.Schema<ICity> = yup.object().shape({
+  name: yup.string().required().min(3),
+  state: yup.string().required().min(3)
+});
 
 // Dessa maneira se declara que a requesição necesssita do recebimento do terceiro campo(body) o ICity
-export const createCityController = (req: Request<{}, {}, ICity>, res: Response) => {
+export const createCityController = async (req: Request<{}, {}, ICity>, res: Response) => {
+
+  let validateData: ICity | undefined = undefined;
 
   const data = req.body;
+
+  try{
+    validateData = await bodyValidation.validate(data);
+  } catch(err){
+    const yupError = err as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message,
+      }
+    });
+  }
+
 
   return res.send('Create!');
 };
