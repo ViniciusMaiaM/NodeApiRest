2ac6f7b7c912d5b28fafe53c0fd199cfdd17e667
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-31 09:38:53 -0300

    feat(yup): Add yup translations to pt

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
-import { router } from './routes';
 import 'dotenv/config';
+
+import './shared/services/TranslationsYup';
+import { router } from './routes';
 
 const server = express();
 
