abedf9206a417ef6ea904f8257c6c21243997ae1
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-31 10:35:04 -0300

    feat(cities): Add query validator

--- src/server/controllers/Cities/createCityController.ts
+++ src/server/controllers/Cities/createCityController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, RequestHandler, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 // Yup é um package para schema e validação em tempo de execução
 import * as yup from 'yup';
@@ -13,15 +13,18 @@ const bodyValidation: yup.Schema<ICity> = yup.object().shape({
   state: yup.string().required().min(3)
 });
 
-// Dessa maneira se declara que a requesição necesssita do recebimento do terceiro campo(body) o ICity
-export const createCityController = async (req: Request<{}, {}, ICity>, res: Response) => {
+interface IFilter{
+  filter?: string;
+}
 
-  let validatedData: ICity | undefined = undefined;
+const queryValidation: yup.Schema<IFilter> = yup.object().shape({
+  filter: yup.string().required().min(3)
+});
 
-  const data = req.body;
-
+export const createQueryValidator: RequestHandler = async (req, res, next) => {
   try{
-    validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
+    await queryValidation.validate(req.body, { abortEarly: false });
+    return next();
   } catch(err){
     const yupError = err as yup.ValidationError;
     const errors: Record<string, string> = {};
@@ -37,7 +40,34 @@ export const createCityController = async (req: Request<{}, {}, ICity>, res: Res
       errors,
     });
   }
+}; 
 
+// Ao receber uma chamada é feita uma validação do body dela
+export const createBodyValidator: RequestHandler = async (req, res, next) => {
+  try{
+    await bodyValidation.validate(req.body, { abortEarly: false });
+    return next();
+  } catch(err){
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
+
+    // Mapeação e agrupamento de todos os erros que aconteceram
+    yupError.inner.forEach(error => {
+      // Caminho para o atributo do erro e a mensagem
+      if(error.path === undefined) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors,
+    });
+  }
+}; 
+
+// Dessa maneira se declara que a requesição necesssita do recebimento do terceiro campo(body) o ICity
+export const createCityController = async (req: Request<{}, {}, ICity>, res: Response) => {
+
+  const data = req.body;
 
   return res.send('Create!');
 };
