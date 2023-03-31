e06d20259028cf3b57f52a47d49ddb26ce2add79
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-31 09:38:32 -0300

    feat(city): Add abortearly and error validation

--- src/server/controllers/Cities/createCityController.ts
+++ src/server/controllers/Cities/createCityController.ts
@@ -16,19 +16,25 @@ const bodyValidation: yup.Schema<ICity> = yup.object().shape({
 // Dessa maneira se declara que a requesição necesssita do recebimento do terceiro campo(body) o ICity
 export const createCityController = async (req: Request<{}, {}, ICity>, res: Response) => {
 
-  let validateData: ICity | undefined = undefined;
+  let validatedData: ICity | undefined = undefined;
 
   const data = req.body;
 
   try{
-    validateData = await bodyValidation.validate(data);
+    validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
   } catch(err){
     const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
 
-    return res.json({
-      errors: {
-        default: yupError.message,
-      }
+    // Mapeação e agrupamento de todos os erros que aconteceram
+    yupError.inner.forEach(error => {
+      // Caminho para o atributo do erro e a mensagem
+      if(error.path === undefined) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors,
     });
   }
 
