26ed9e17ba1f5c04dad54596493b2780eec0a421
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-04-01 16:45:28 -0300

    feat(controllers): removing validations from controllers and passing to middleware

--- src/server/controllers/Cities/createCityController.ts
+++ src/server/controllers/Cities/createCityController.ts
@@ -2,6 +2,7 @@ import { Request, RequestHandler, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 // Yup é um package para schema e validação em tempo de execução
 import * as yup from 'yup';
+import { validation } from '../../shared/middlewares';
 
 interface ICity{
   name: string;
@@ -21,48 +22,17 @@ const queryValidation: yup.Schema<IFilter> = yup.object().shape({
   filter: yup.string().required().min(3)
 });
 
-export const createQueryValidator: RequestHandler = async (req, res, next) => {
-  try{
-    await queryValidation.validate(req.body, { abortEarly: false });
-    return next();
-  } catch(err){
-    const yupError = err as yup.ValidationError;
-    const errors: Record<string, string> = {};
-
-    // Mapeação e agrupamento de todos os erros que aconteceram
-    yupError.inner.forEach(error => {
-      // Caminho para o atributo do erro e a mensagem
-      if(error.path === undefined) return;
-      errors[error.path] = error.message;
-    });
-
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      errors,
-    });
-  }
-}; 
-
 // Ao receber uma chamada é feita uma validação do body dela
-export const createBodyValidator: RequestHandler = async (req, res, next) => {
-  try{
-    await bodyValidation.validate(req.body, { abortEarly: false });
-    return next();
-  } catch(err){
-    const yupError = err as yup.ValidationError;
-    const errors: Record<string, string> = {};
 
-    // Mapeação e agrupamento de todos os erros que aconteceram
-    yupError.inner.forEach(error => {
-      // Caminho para o atributo do erro e a mensagem
-      if(error.path === undefined) return;
-      errors[error.path] = error.message;
-    });
-
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      errors,
-    });
-  }
-}; 
+export const createValidation = validation({
+  body: yup.object().shape({
+    nome: yup.string().required().min(3),
+    state: yup.string().required().min(3),
+  }),
+  query: yup.object().shape({
+    filter: yup.string().required().min(3)
+  }),
+});
 
 // Dessa maneira se declara que a requesição necesssita do recebimento do terceiro campo(body) o ICity
 export const createCityController = async (req: Request<{}, {}, ICity>, res: Response) => {
