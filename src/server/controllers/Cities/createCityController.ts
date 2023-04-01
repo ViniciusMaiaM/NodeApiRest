42831bbf8479ba243208170c6461c9e8418131c8
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-04-01 20:37:07 -0300

    feat(controller): removing validation from controller

--- src/server/controllers/Cities/createCityController.ts
+++ src/server/controllers/Cities/createCityController.ts
@@ -1,5 +1,4 @@
-import { Request, RequestHandler, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import { Request, Response } from 'express';
 // Yup é um package para schema e validação em tempo de execução
 import * as yup from 'yup';
 import { validation } from '../../shared/middlewares';
@@ -9,35 +8,24 @@ interface ICity{
   state: string;
 }
 
-const bodyValidation: yup.Schema<ICity> = yup.object().shape({
-  name: yup.string().required().min(3),
-  state: yup.string().required().min(3)
-});
-
 interface IFilter{
   filter?: string;
 }
 
-const queryValidation: yup.Schema<IFilter> = yup.object().shape({
-  filter: yup.string().required().min(3)
-});
-
 // Ao receber uma chamada é feita uma validação do body dela
 
-export const createValidation = validation({
-  body: yup.object().shape({
-    nome: yup.string().required().min(3),
+export const createValidation = validation((getSchema) => ({
+  body: getSchema<ICity>(yup.object().shape({
+    name: yup.string().required().min(3),
     state: yup.string().required().min(3),
-  }),
-  query: yup.object().shape({
+  })),
+  query: getSchema<IFilter>(yup.object().shape({
     filter: yup.string().required().min(3)
-  }),
-});
+  })),
+}));
 
 // Dessa maneira se declara que a requesição necesssita do recebimento do terceiro campo(body) o ICity
 export const createCityController = async (req: Request<{}, {}, ICity>, res: Response) => {
-
-  const data = req.body;
 
   return res.send('Create!');
 };
