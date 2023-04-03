4c695973ec783e38dff00705a4ca35e0cb23cb90
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-04-03 12:26:16 -0300

    refactor(validation): refactoring the yup validations

--- src/server/shared/middlewares/Validation.ts
+++ src/server/shared/middlewares/Validation.ts
@@ -1,42 +1,42 @@
 import { RequestHandler } from 'express';
+import { AnyObject, Maybe, ObjectSchema, ValidationError } from 'yup';
 import { StatusCodes } from 'http-status-codes';
-import { Schema, ValidationError } from 'yup';
 
-// Para declarar que ele é uma função que retorna RequestHandler é mais fácil e mais legível utilizar o type ao inves de interface
-type TValidation = (getAllSchemas: TGetAllSchemas) => RequestHandler;
 
 type TProperty = 'body' | 'header' | 'params' | 'query';
 
-type TallSchemas = Record<TProperty, Schema<any>>;
+type TGetSchema = <T extends Maybe<AnyObject>>(schema: ObjectSchema<T>) => ObjectSchema<T>
 
-type TGetSchema = <T>(schema: Schema<T>) => Schema<T>;
+type TAllSchemas = Record<TProperty, ObjectSchema<any>>;
 
-type TGetAllSchemas = (getSchema: TGetSchema) => Partial<TallSchemas>;
+type TGetAllSchemas = (getSchema: TGetSchema) => Partial<TAllSchemas>;
+
+type TValidation = (getAllSchemas: TGetAllSchemas) => RequestHandler;
 
 export const validation: TValidation = (getAllSchemas) => async (req, res, next) => {
   const schemas = getAllSchemas((schema) => schema);
 
+
   const errorsResult: Record<string, Record<string, string>> = {};
 
   Object.entries(schemas).forEach(([key, schema]) => {
-    try{
+    try {
       schema.validateSync(req[key as TProperty], { abortEarly: false });
-    } catch(err){
+    } catch (err) {
       const yupError = err as ValidationError;
       const errors: Record<string, string> = {};
-  
-      // Mapeação e agrupamento de todos os erros que aconteceram
+
       yupError.inner.forEach(error => {
-        // Caminho para o atributo do erro e a mensagem
-        if(error.path === undefined) return;
+        if (error.path === undefined) return;
         errors[error.path] = error.message;
       });
-      
-      errorsResult[key as TProperty] = errors;
+
+      errorsResult[key] = errors;
     }
   });
 
-  if (Object.entries(errorsResult).length === 0){
+
+  if (Object.entries(errorsResult).length === 0) {
     return next();
   } else {
     return res.status(StatusCodes.BAD_REQUEST).json({ errors: errorsResult });
