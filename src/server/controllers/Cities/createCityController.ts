b6f0437f8a4c87d6372589b46b8b9e0207255f42
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-04-03 12:28:14 -0300

    feat(getall): adding getAllcities controller

--- src/server/controllers/Cities/createCityController.ts
+++ src/server/controllers/Cities/createCityController.ts
@@ -2,30 +2,22 @@ import { Request, Response } from 'express';
 // Yup é um package para schema e validação em tempo de execução
 import * as yup from 'yup';
 import { validation } from '../../shared/middlewares';
+import { StatusCodes } from 'http-status-codes';
 
 interface ICity{
   name: string;
-  state: string;
-}
-
-interface IFilter{
-  filter?: string;
 }
 
 // Ao receber uma chamada é feita uma validação do body dela
 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<ICity>(yup.object().shape({
-    name: yup.string().required().min(3),
-    state: yup.string().required().min(3),
-  })),
-  query: getSchema<IFilter>(yup.object().shape({
-    filter: yup.string().required().min(3)
-  })),
+    name: yup.string().required().min(3)
+  }))
 }));
 
 // Dessa maneira se declara que a requesição necesssita do recebimento do terceiro campo(body) o ICity
 export const createCityController = async (req: Request<{}, {}, ICity>, res: Response) => {
 
-  return res.send('Create!');
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado!');
 };
--- src/server/controllers/Cities/index.ts
+++ src/server/controllers/Cities/index.ts
@@ -1,7 +1,9 @@
 // Arquivo responsavel de agrupar todas as controllers em uma única variável
 // Dessa maneira conseguimos todos os atributos presentes no controller
 import * as createCityController from './createCityController';
+import * as getAllController from './getAllController';
 
 export const CitiesController = {
   ...createCityController,
+  ...getAllController,
 };
